59055d31f6ef182c87b7c67129637a91a6cbc389
AliHamdi63 <aawere1233@gmail.com>
2024-01-11 17:18:32 +0200

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { Header, Nav, About, Contact, Footer, Portfolio } from "./components/"
+
+const App = () => {
+
+
+    return (
+        <>
+            <Header />
+            <Nav />
+            <About />
+
+
+            <Portfolio />
+
+            <Contact />
+            <Footer />
+        </>
+    )
+}
+
+export default App
