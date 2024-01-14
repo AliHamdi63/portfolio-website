6bd97861392de408a05e13b4ee2b8d6e66a3ccf2
AliHamdi63 <aawere1233@gmail.com>
2024-01-14 23:59:44 +0200

    modify

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import { FaGithub } from "react-icons/fa"
 const HeaderSocials = () => {
     return (
         <div className='header__socials'>
-            <a href='https://github.com/AliHamdi63?tab=repositories' target='_blank' className=''><BsLinkedin /></a>
-            <a href='https://www.linkedin.com/in/ali-hamdi8/' target='_blank' className=''> <FaGithub /></a>
+            <a href='https://www.linkedin.com/in/ali-hamdi8/' target='_blank' className=''><BsLinkedin /></a>
+            <a href='https://github.com/AliHamdi63?tab=repositories' target='_blank' className=''> <FaGithub /></a>
             {/* <a href='' target='_blank' className=''></a> */}
         </div>
     )
