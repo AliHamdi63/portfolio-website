54c341f5fda0eb86fc254333f313e5bccd262331
AliHamdi63 <aawere1233@gmail.com>
2024-01-15 15:25:58 +0200

    modify

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -59,7 +59,7 @@ const Experience = () => {
                             <BsPatchCheckFill className='experience__details-icon' />
                             <div>
                                 <h4>Angular</h4>
-                                <small className='text-light'>Intermediate</small>
+                                <small className='text-light'>Basic</small>
                             </div>
                         </article>
                         <article className='experience__details'>
